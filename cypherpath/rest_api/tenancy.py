62655ee9f698fc087500be1127476015fe1378d1
craig <craig.stevenson@gmail.com>
2018-07-11 03:37:27 -0700

    added tenancy

--- cypherpath/rest_api/tenancy.py
+++ cypherpath/rest_api/tenancy.py
@@ -1,5 +1,14 @@
+from cypherpath.rest_api.client import Client
+import requests
+import urllib3
+urllib3.disable_warnings()
+
+
 def get_tenancies():
-    pass
+    client = Client()
+    api_url = f'https://{client.url}/api/accounts/tenancies/'
+    response = requests.get(api_url, headers=client.headers, verify=False)
+    return response
 
 def get_tenancy():
     pass
--- examples.py
+++ examples.py
@@ -1,13 +1,17 @@
 from cypherpath.rest_api import user
 from cypherpath.rest_api import sdi
+from cypherpath.rest_api import tenancy
+import json
+
+
+def test_tenancy():
+    response = tenancy.get_tenancies()
+    print('status_code = {}'.format(response.status_code))
+    parsed = json.loads(response.text)
+    print('json() = {}'.format(json.dumps(parsed, indent=4, sort_keys=False)))
 
 if __name__ == "__main__":
-    response = sdi.get_sdi_by_user(6)
-    print('status_code = {}'.format(response.status_code))
-    print('json() = {}'.format(response.json()))
-
-    for r in response.json():
-        print(f'{r["name"]} = {r["sdi_id"]}')
+    test_tenancy()
 
 
 
